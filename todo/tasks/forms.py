0cbc8bdf9f5ac853d2ade4b58aaaeeedfcf0a342
chabtj <tejasvichabbra@yahoo.in>
2020-05-29 17:33:14 +0530

    frontend

--- todo/tasks/forms.py
+++ todo/tasks/forms.py
@@ -1,9 +1,11 @@
+from django.forms import ModelForm
 from django import forms
 from .models import Task
-class TaskForm(forms.ModelForm):
-    class Meta:
-        model = Task
-        fields = '__all__'
-        labels = {
-                    "title": "Add Task"
-                 }
+class TaskForm(ModelForm):
+	title=forms.CharField(max_length=100)
+	title.widget.attrs.update({'style':'color:#4C2719;  border=3px solid black; width:500px; font-size:24px; padding:10px; background:#EDE580;'})
+	completed=forms.BooleanField()
+	completed.widget.attrs.update({'style':'width:50px; height:20px; background:#EDE580;'})
+	class Meta :
+		model=Task
+		fields=['title','completed']
